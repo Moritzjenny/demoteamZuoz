fd9e75291d51ccf60b05c2a54501c5e82d456d01
Moritzjenny <moritzjenny@hotmail.com>
2023-12-14 20:39:19 +0100

    new public path

--- quasar.config.js
+++ quasar.config.js
@@ -68,7 +68,7 @@ module.exports = configure(function (/* ctx */) {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      // publicPath: '/',
+      publicPath: '/demoteamZuoz/',
       // analyze: true,
       // env: {},
       // rawDefine: {}
