0076eafced27c817e5b0d8919e87ff3f7a7159b6
Moritzjenny <moritzjenny@hotmail.com>
2023-12-15 08:04:12 +0100

    redeploy

--- quasar.config.js
+++ quasar.config.js
@@ -67,7 +67,7 @@ module.exports = configure(function (/* ctx */) {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-       publicPath: '/demoteamZuoz/',
+      publicPath: '/demoteamZuoz/',
       // analyze: true,
       // env: {},
       // rawDefine: {}
